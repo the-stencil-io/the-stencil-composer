62c7efc1f15d1ba99c0a10263105f5f055cdfa79
Jocelyn Mutso (ext) <jocelyn.mutso@resys.io>
2021-10-07 11:57:55 +0300

    add order numbers to explorer items: Articles

--- src/core/composers/link/LinkTable.tsx
+++ src/core/composers/link/LinkTable.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import { Theme, Typography } from '@mui/material';
 import { makeStyles, createStyles } from '@mui/styles';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import {
+  Theme, Typography, Table, TableBody, TableCell,
+  TableContainer, TableRow, TableHead, Paper
+} from '@mui/material';
+
 import { FormattedMessage } from 'react-intl';
 
 import { API, Ide } from '../../deps';
@@ -58,29 +55,30 @@ const LinkTable: React.FC<LinkTableProps> = ({ article }) => {
 
   return (
     <>
-   <Typography variant="h3" className={classes.title}>{article.body.name}{": "}<FormattedMessage id="links" /> </Typography>
-    <TableContainer component={Paper}>
-      <Table className={classes.table} size="small" >
-        <TableHead>
-          <TableRow>
-            <TableCell className={classes.bold} align="left"><FormattedMessage id="link.type"/></TableCell>
-            <TableCell className={classes.bold} align="left"><FormattedMessage id="locale"/></TableCell>
-            <TableCell className={classes.bold} align="left"><FormattedMessage id="description"/></TableCell>
-            <TableCell className={classes.bold} align="left"><FormattedMessage id="value"/></TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {links.map((link, index) => (
-            <TableRow hover key={index}>
-              <TableCell className={classes.tableCell} align="left">{link.body.contentType}</TableCell>
-              <TableCell className={classes.tableCell} align="left">{site.locales[link.body.locale].body.value}</TableCell>
-              <TableCell className={classes.tableCell} align="left">{link.body.description}</TableCell>
-              <TableCell className={classes.tableCell} align="left">{link.body.content}</TableCell>
+    alrlaru
+      <Typography variant="h3" className={classes.title}>{article.body.name}{": "}<FormattedMessage id="links" /> </Typography>
+      <TableContainer component={Paper}>
+        <Table className={classes.table} size="small" >
+          <TableHead>
+            <TableRow>
+              <TableCell className={classes.bold} align="left"><FormattedMessage id="link.type" /></TableCell>
+              <TableCell className={classes.bold} align="left"><FormattedMessage id="locale" /></TableCell>
+              <TableCell className={classes.bold} align="left"><FormattedMessage id="description" /></TableCell>
+              <TableCell className={classes.bold} align="left"><FormattedMessage id="value" /></TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer >
+          </TableHead>
+          <TableBody>
+            {links.map((link, index) => (
+              <TableRow hover key={index}>
+                <TableCell className={classes.tableCell} align="left">{link.body.contentType}</TableCell>
+                <TableCell className={classes.tableCell} align="left">{site.locales[link.body.locale].body.value}</TableCell>
+                <TableCell className={classes.tableCell} align="left">{link.body.description}</TableCell>
+                <TableCell className={classes.tableCell} align="left">{link.body.content}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer >
     </>
   );
 }
