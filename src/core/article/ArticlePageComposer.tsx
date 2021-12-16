985b44ab33a79c9e40bf2143f380ea91a4bc5e75
Jocelyn Mutso (ext) <jocelyn.mutso@resys.io>
2021-12-16 17:49:35 +0200

    add template insert at cursor functionality

--- src/core/article/ArticlePageComposer.tsx
+++ src/core/article/ArticlePageComposer.tsx
@@ -1,26 +1,59 @@
 import React from 'react';
 import { Box, useTheme } from '@mui/material';
 
-import MDEditor, { ICommand, getCommands } from '@uiw/react-md-editor';
+import MDEditor, { ICommand, commands, TextState, TextAreaTextApi } from '@uiw/react-md-editor';
 import { Composer, StencilClient } from '../context';
 
 
-const getMdCommands = (locale: StencilClient.SiteLocale, color: string) => {
+const templateCommand = (template: StencilClient.Template): ICommand => ({
+  name: 'templates' + template.id,
+  keyCommand: 'templates' + template.id,
+  buttonProps: { 'aria-label': template.body.name, title: template.body.name },
+  icon: <div style={{ fontSize: 18, padding: 10, textAlign: 'left' }}>{template.body.name} - {template.body.description}</div>,
+  execute: (state: TextState, api: TextAreaTextApi) => {
+    api.replaceSelection(template.body.content);
+  },
+});
+
+const getMdCommands = (locale: StencilClient.SiteLocale, color: string, site: StencilClient.Site) => {
   const localeTitle: ICommand = {
     name: locale.body.value,
     groupName: 'title',
     keyCommand: 'title1',
     buttonProps: { 'aria-label': locale.body.value },
-    icon: (<span style={{
-      color: color,
-      fontWeight: 'bold',
-      fontSize: '15pt',
-      textAlign: 'center',
-      textTransform: 'uppercase',
-    }}>{locale.body.value}</span>),
+    icon: (<div style={{ fontWeight: 'bold', fontSize: 15, alignItems: 'center', color }}>{locale.body.value}</div>)
+
   };
 
-  return [localeTitle, ...getCommands()];
+
+  return [localeTitle,
+    commands.group(Object.values(site.templates).map((t) => templateCommand(t)), {
+      name: 'templates',
+      groupName: 'templates',
+      buttonProps: { 'aria-label': 'Insert Template' },
+      icon: (<div style={{ fontWeight: 'bold', fontSize: 15, alignItems: 'center' }}>T</div>)
+    }),
+    commands.group([commands.title1, commands.title2, commands.title3, commands.title4, commands.title5, commands.title6], {
+      name: 'title',
+      groupName: 'title',
+      buttonProps: { 'aria-label': 'Insert title' },
+
+    }),
+    commands.bold,
+    commands.italic,
+    commands.strikethrough,
+    commands.hr,
+    commands.divider,
+    commands.link,
+    commands.quote,
+    commands.code,
+    commands.codeBlock,
+    commands.image,
+    commands.divider,
+    commands.unorderedListCommand,
+    commands.orderedListCommand,
+    commands.checkedListCommand,
+  ];
 }
 
 type PageComposerProps = {
@@ -31,9 +64,10 @@ type PageComposerProps = {
 
 const ArticlePageComposer: React.FC<PageComposerProps> = ({ articleId, locale1, locale2 }) => {
   const theme = useTheme();
-  const {actions, session} = Composer.useComposer();
+  const { actions, session } = Composer.useComposer();
+  const { site } = session;
   const view = session.getArticleView(articleId);
-  
+
   const page1 = [...view.pages.map(p => p.page)]
     .filter(p => p.body.locale === locale1).pop() as StencilClient.Page;
 
@@ -50,11 +84,11 @@ const ArticlePageComposer: React.FC<PageComposerProps> = ({ articleId, locale1, 
   if (value2 === undefined || !page2) {
     return (
       <div>
-        <MDEditor key={1} value={value1} onChange={handleChange1}
-          commands={getMdCommands(session.site.locales[page1.body.locale], theme.palette.page.main)}
+        <MDEditor key={1} value={value1} onChange={handleChange1} toolbarHeight={40}
+          commands={getMdCommands(session.site.locales[page1.body.locale], theme.palette.page.main, site)}
           textareaProps={{ placeholder: '# Title' }}
-          height={800} 
-          />
+          height={800}
+        />
       </div>
     );
   }
@@ -67,18 +101,18 @@ const ArticlePageComposer: React.FC<PageComposerProps> = ({ articleId, locale1, 
     <Box display="flex" flexDirection="row" flexWrap="wrap">
       <Box flex="1" sx={{ paddingRight: 1 }}>
         <MDEditor key={2} value={value1} onChange={handleChange1}
-          commands={getMdCommands(session.site.locales[page1.body.locale], theme.palette.page.main)}
+          commands={getMdCommands(session.site.locales[page1.body.locale], theme.palette.page.main, site)}
           textareaProps={{ placeholder: '# Title' }}
-          height={800} 
-          />
+          height={800}
+        />
 
       </Box>
       <Box flex="1">
         <MDEditor key={3} value={value2} onChange={handleChange2}
-          commands={getMdCommands(session.site.locales[page2.body.locale], theme.palette.page.dark)}
+          commands={getMdCommands(session.site.locales[page2.body.locale], theme.palette.page.dark, site)}
           textareaProps={{ placeholder: '# Title' }}
-          height={800} 
-          />
+          height={800}
+        />
 
       </Box>
     </Box>
