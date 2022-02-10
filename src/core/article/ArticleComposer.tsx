7c8d25a7adaaf1d76b26afd0d677bcbf449eec2d
Jocelyn Mutso (ext) <jocelyn.mutso@resys.io>
2022-02-10 13:27:54 +0200

    order articles by order number in article composer

--- src/core/article/ArticleComposer.tsx
+++ src/core/article/ArticleComposer.tsx
@@ -24,26 +24,38 @@ const ArticleComposer: React.FC<{ onClose: () => void }> = ({ onClose }) => {
   }
 
   const articles: { id: string, value: string }[] = Object.values(session.site.articles)
+    .filter((a) => !a.body.parentId)
+    .sort((a1, a2) => {
+      if (a1.body.parentId && a1.body.parentId === a2.body.parentId) {
+        const children = a1.body.order - a2.body.order;
+        if (children === 0) {
+          return a1.body.name.localeCompare(a2.body.name);
+        }
+        return children;
+      }
+
+      return (a1.body.parentId ? session.site.articles[a1.body.parentId].body.order + 1 : a1.body.order)
+        - (a2.body.parentId ? session.site.articles[a2.body.parentId].body.order + 1 : a2.body.order);
+    })
     .map(article => ({
       id: article.id,
-      value: `${article.body.name}`
+      value: `${article.body.order} - ${article.body.name}`,
     }));
-
-
+    
   return (
     <StencilStyles.Dialog open={true} onClose={onClose}
       backgroundColor="uiElements.main" title="article.composer.title"
       submit={{ title: "article.create", onClick: handleCreate, disabled: !name }}>
       <>
-
+       
         <StencilStyles.Select label="article.composer.parent"
           helperText={"article.parent.helper"}
           selected={parentId} 
           onChange={setParentId}
           empty={{ id: DUMMY_ID, label: 'article.composer.parent.unselected' }}
-          items={articles.map((article) => ({
+          items={articles.map((article) => ({ 
             id: article.id,
-            value: (<ListItemText primary={article.value} />)
+            value: (<ListItemText primary={article.value}/>)
           }))}
         />
 
