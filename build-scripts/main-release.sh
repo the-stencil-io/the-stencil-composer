055b761df87d77058b0dcfc5ac88a7c1ae8a4830
Jocelyn Mutso <47987958+jocelynmutso@users.noreply.github.com>
2022-08-23 17:28:03 +0300

    Update main-release.sh

--- build-scripts/main-release.sh
+++ build-scripts/main-release.sh
@@ -10,41 +10,25 @@ if [[ "${last_release_commit_hash}" = "${GITHUB_SHA}" ]]; then
      #exit 0
 fi
 
-
 # Config GIT
 echo "Setup git user name to '$BOT_NAME' and email to '$BOT_EMAIL'"
 git config --global user.name "$BOT_NAME";
 git config --global user.email "$BOT_EMAIL";
 
-# Checkout
-git reset --hard
-git fetch --all
-git branch -a --contains ${GITHUB_SHA} --format="%(refname)"
-
-
-readonly local refname=$(git branch -a --contains ${GITHUB_SHA} --format="%(refname)" | head -1)
-if [[ "${refname}" = "refs/heads/main" ]]; then
-     readonly local branch="main"
-else
-     readonly local branch=${refname#refs/remotes/origin/}
-fi
-
 # resolve versions
-git checkout ${branch}
 readonly local PROJECT_VERSION=$(node -e "console.log(require('./package.json').version);")
 yarn version --patch
 readonly local PROJECT_VERSION_NEXT=$(node -e "console.log(require('./package.json').version);")
 
 # Log
-echo "Git checkout refname: '${refname}' branch: '${branch}' commit: '${GITHUB_SHA}'"
 echo "Project version: '${PROJECT_VERSION}' next: '${PROJECT_VERSION_NEXT}'"
 
 # Tag and publish
-#yarn install
-#yarn build
-#yarn publish --new-version ${PROJECT_VERSION_NEXT}  --access public
+yarn install
+yarn build
+yarn publish --new-version ${PROJECT_VERSION_NEXT}  --access public
 
-#git push origin ${branch}
+git push origin ${branch}
 
- git tag -a ${PROJECT_VERSION_NEXT} -m "release ${PROJECT_VERSION_NEXT}"
- git push origin --tags
+# git tag -a ${PROJECT_VERSION_NEXT} -m "release ${PROJECT_VERSION_NEXT}"
+# git push origin --tags
