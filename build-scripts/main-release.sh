938f2436a8abeb7fa0edcddfab7f83ac07959b7c
Olev Mutso <olev.mutso@resys.io>
2023-02-17 13:16:33 +0200

    Update main-release.sh

--- build-scripts/main-release.sh
+++ build-scripts/main-release.sh
@@ -15,24 +15,38 @@ echo "Setup git user name to '$BOT_NAME' and email to '$BOT_EMAIL'"
 git config --global user.name "$BOT_NAME";
 git config --global user.email "$BOT_EMAIL";
 
+# yarn
+corepack enable
+yarn set version 3.1.1
+echo "Current yarn version: $(yarn -v), running install and build"
+
 # resolve versions
 readonly local PROJECT_VERSION=$(node -e "console.log(require('./package.json').version);")
-yarn version --no-git-tag-version --patch
-readonly local PROJECT_VERSION_NEXT=$(node -e "console.log(require('./package.json').version);")
-
-# Log
-echo "Project version: '${PROJECT_VERSION}' next: '${PROJECT_VERSION_NEXT}'"
 NEWLINE=$'\n'
 DATE=$(date +"%d/%m/%Y")
 echo "const version = {tag: '${PROJECT_VERSION_NEXT}', built: '${DATE}'};${NEWLINE}export default version;" > ./src/core/version.ts
-
-# Tag and publish
+echo "Project version: '${PROJECT_VERSION}'"
+git commit -am "release: update version.ts"
 yarn install
 yarn build
-yarn publish --new-version ${PROJECT_VERSION_NEXT}  --access public
+
+
+# Publish and Tag
+git tag -a ${PROJECT_VERSION} -m "release: '${PROJECT_VERSION}'"
+yarn npm publish --access public
+git push origin --tags
+
+# Next
+git update-index --assume-unchanged .yarnrc.yml
+yarn plugin import version
+yarn version patch
 
 git commit -am "release ${PROJECT_VERSION_NEXT}"
 git tag -a ${PROJECT_VERSION_NEXT} -m "release ${PROJECT_VERSION_NEXT}"
 git push origin --tags
 git push origin main
 
+readonly local PROJECT_VERSION_NEXT=$(node -e "console.log(require('./package.json').version);")
+git commit -am "release: '${PROJECT_VERSION_NEXT}'"
+git push origin main
+echo "Released: '${PROJECT_VERSION}', now: '${PROJECT_VERSION_NEXT}'"
