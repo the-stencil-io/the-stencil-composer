b2eab918c9e00121ad5fee930848b1d5fa60495d
Olev Mutso <olev.mutso@resys.io>
2023-02-17 14:02:49 +0200

    Update main-release.sh

--- build-scripts/main-release.sh
+++ build-scripts/main-release.sh
@@ -20,3 +20,32 @@ corepack enable
 yarn set version 3.1.1
 echo "Current yarn version: $(yarn -v), running install and build"
 
+# resolve versions
+readonly local PROJECT_VERSION=$(node -e "console.log(require('./package.json').version);")
+NEWLINE=$'\n'
+DATE=$(date +"%d/%m/%Y")
+echo "const version = {tag: '${PROJECT_VERSION_NEXT}', built: '${DATE}'};${NEWLINE}export default version;" > ./src/core/version.ts
+echo "Project version: '${PROJECT_VERSION}'"
+git commit -am "release: update version.ts"
+yarn install --no-immutable
+yarn build
+
+# Publish and Tag
+git tag -a ${PROJECT_VERSION} -m "release: '${PROJECT_VERSION}'"
+yarn npm publish --access public
+git push origin --tags
+
+# Next
+git update-index --assume-unchanged .yarnrc.yml
+yarn plugin import version
+yarn version patch
+
+git commit -am "release ${PROJECT_VERSION_NEXT}"
+git tag -a ${PROJECT_VERSION_NEXT} -m "release ${PROJECT_VERSION_NEXT}"
+git push origin --tags
+git push origin main
+
+readonly local PROJECT_VERSION_NEXT=$(node -e "console.log(require('./package.json').version);")
+git commit -am "release: '${PROJECT_VERSION_NEXT}'"
+git push origin main
+echo "Released: '${PROJECT_VERSION}', now: '${PROJECT_VERSION_NEXT}'"
